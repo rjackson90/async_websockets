2ff07e6ec9d322f97fabe4df19e6ae3b4dc0adef
Richard Jackson <rjackson90@gmail.com>
2016-10-20 23:05:10 -0500

    Added first tests for simple framing
    I took a couple of example frames from RFC 6455 and made tests! As of now, we can
    be confident that the parser can grok simple text messages.
    
    Now that unit tests have been bootstrapped, I will make an effort to include them from here on out.

--- src/frames.rs
+++ src/frames.rs
@@ -8,7 +8,8 @@ use byteorder::BigEndian;
 
 type Frame = pipeline::Frame<WsFrame, WsFrame, io::Error>;
 
-enum WsFrame {
+#[derive(Debug)]
+pub enum WsFrame {
 	// Data Frames
 	Text { payload: String },
 	Binary { payload: Vec<u8> },
@@ -18,7 +19,7 @@ enum WsFrame {
 	Pong { payload: Vec<u8> }
 }
 
-struct Parser;
+pub struct Parser;
 
 impl Parse for Parser {
 	type Out = Frame;
@@ -40,7 +41,7 @@ impl Parse for Parser {
 			return None;
 		}
 
-		let fin = data[0] & 0x80 == 1;	// The 0th bit is the FIN flag
+		let fin = data[0] & 0x80 == 0x80;	// The 0th bit is the FIN flag
 		let opcode = data[0] & 0x0f;	// Bits 4-7 are the opcode
 		if data[0] & 0x70 != 0 {
 			// Right now this server supports no extensions, so if any
@@ -48,7 +49,7 @@ impl Parse for Parser {
 			return Some(Error(io::Error::new(io::ErrorKind::Other, "Unsupported extension")));
 		}
 
-		let mask = data[1] & 0x80 == 1; // The 8th bit is the MASK flag
+		let mask = data[1] & 0x80 == 0x80; // The 8th bit is the MASK flag
 		if !mask {
 			// The spec says ALL client frames MUST be masked
 			return Some(Error(io::Error::new(io::ErrorKind::Other, "Client input is unmasked")));			
@@ -68,8 +69,11 @@ impl Parse for Parser {
 
 		// Unmask data
 		for n in 0..len as usize{
-			payload[n] = cursor.read_u8() ^ mask_key[n % 4];
+			payload.insert(n, cursor.read_u8() ^ mask_key[n % 4]);
 		}
+
+		// Drop every byte that has been read
+		buf.drop(cursor.position() as usize);
 
 		// Return a different message type depending on the opcode
 		match opcode {
@@ -87,7 +91,7 @@ impl Parse for Parser {
 	}
 }
 
-struct Serializer;
+pub struct Serializer;
 
 impl Serialize for Serializer {
 	type In = Frame;
@@ -99,9 +103,66 @@ impl Serialize for Serializer {
 
 #[cfg(test)]
 mod tests {
+	use super::*;
+	use bytes::buf::BlockBuf;
+	use bytes::MutBuf;
+	use tokio_proto::Parse;
+	use tokio_proto::pipeline::Frame::{Message, Error};
 
 	#[test]
 	fn mod_good() {
 		assert!(true)
+	}
+
+	#[test]
+	fn single_unmasked_text() {
+		let mut test_buf = BlockBuf::default();
+		let mut test_parser = Parser {};
+		// Taken from RFC 6455 as an example of a text message containing 'Hello'
+		let message = vec![	0x81, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6f];
+
+		test_buf.write_slice(&message);
+		let result = test_parser.parse(&mut test_buf);
+		assert!(result.is_some(), "Failed to parse message");
+
+		match result.unwrap() {
+			Error(err) => assert!(true),
+			data => assert!(false, "expected an error, got {:?}", data)
+		};
+		
+		let remaining = test_buf.len();
+		assert!(remaining == message.len(), "Buffer has {} un-consumed bytes after parse", remaining);
+	}
+
+	#[test]
+	fn single_masked_text() {
+		let mut test_buf = BlockBuf::default();
+		let mut test_parser = Parser {};
+		// Taken from RFC 6455 as an example of a text message containing 'Hello'
+		let message = vec![	0x81, 0x85, 0x37, 0xfa, 0x21, 0x3d, 0x7f, 0x9f, 0x4d, 0x51, 0x58 ];
+
+		test_buf.write_slice(&message);
+		let result = test_parser.parse(&mut test_buf);
+		assert!(result.is_some(), "Failed to parse message");
+
+		let message = match result.unwrap() {
+			Message(msg) => msg,
+			err => {
+				assert!(false, "Parser returned error {:?}", err);
+				WsFrame::Close{code: 666, reason: "Bullshit".to_string()}
+			}
+		};
+		let ws_frame = match message {
+			WsFrame::Text{ payload } => payload,
+			err => {
+				assert!(false, "Incorrect WsFrame variant. Got {:?}", err);
+				"Bullshit".to_string()
+			} 
+		};
+		assert!(ws_frame == "Hello");
+
+		let remaining = test_buf.len();
+		assert!(remaining == 0, "Buffer has {} un-consumed bytes after parse", remaining);
+
 	}
 }
